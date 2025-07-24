694d01bce4037ac212bc2e284d23f7d9d4374974
Puneet <puneet932004@gmail.com>
2025-07-24 21:49:44 +0530

    change package.json

--- backend/server.js
+++ backend/server.js
@@ -21,5 +21,5 @@ app.post('/process-emails', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
